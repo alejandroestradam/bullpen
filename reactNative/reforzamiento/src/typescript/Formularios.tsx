ab5e79a3728021ea73ae35f682f1a54eb98c4d6a
Alejandro Estrada <alejandro.estrada.maldonado@hotmail.com>
2022-10-17 13:11:26 -0500

    useform hook created

--- reactNative/reforzamiento/src/typescript/Formularios.tsx
+++ reactNative/reforzamiento/src/typescript/Formularios.tsx
@@ -1,25 +1,19 @@
-import { useState } from "react"
+import { useForms } from "../hooks/useForms"
 
 const Formularios = () => {
-    const [formulario, setFormulario] = useState({
-        email: 'test',
-        password: '123456'
-    })
 
-    const onChange = (value: string, campo: string) =>{
-        setFormulario({
-            ...formulario, 
-            [campo]: value
-        })
-    }
+    const {email, password, onChange} = useForms({
+        email: '',
+        password: ''
+    });
 
     return (
         <div>
             <h3>Formularios</h3>
-            <input type="text" className="form-control" placeholder="Email" value={formulario.email} onChange={(e)=>onChange(e.target.value, 'email')}/>
-            <input type="text" className="form-control mt-2 mb-2" placeholder="Password" value={formulario.password}  onChange={(e)=>onChange(e.target.value, 'password')}/> 
-            <p>{formulario.email}</p>
-            <p>{formulario.password}</p>  
+            <input type="text" className="form-control" placeholder="Email" value={email} onChange={(e)=>onChange(e.target.value, 'email')}/>
+            <input type="text" className="form-control mt-2 mb-2" placeholder="Password" value={password}  onChange={(e)=>onChange(e.target.value, 'password')}/> 
+            <p>{email}</p>
+            <p>{password}</p>  
         </div>
     )
 }
